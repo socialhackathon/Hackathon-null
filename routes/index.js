4c90454b5dd471d0eba06c980513fff4b58b4545
MuratRuslan <muratruslanov@gmail.com>
2017-10-21 10:19:33 +0600

    fdjkslf

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,31 @@ var express = require('express');
 
 var router = express.Router();
 
+
 module.exports = function(app) {
 
-  router.get('/hello', function(req, res) {
+  var db = app.get('db');
+
+  router.get('/', function(req, res) {
     res.render('index.html', {name : "sdlkfjlksaf"});
+
   });
+
+  router.get('/about', function(req, res) {
+    res.render('what.html', {});
+  });
+
+  router.get('/companies', function(req, res) {
+    res.render('donate.html', {});
+  });
+
+  router.get('/reports', function(req, res) {
+    res.render('blog.html', {});
+  });
+
+  router.get('/contact', function(req, res) {
+    res.render('contact.html', {});
+  });
+
   app.use('/', router);
 }
